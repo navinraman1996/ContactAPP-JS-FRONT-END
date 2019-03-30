b436e8b18d76ccfd1321ec5bf3f6376f111bac82
Navin Raman <raman.na@husky.neu.edu>
2019-03-29 22:54:39 -0400

    assignment 7

--- README.md
+++ README.md
@@ -1,23 +1,31 @@
-# scss-webpack-template
+# Web Design and user experience :
+## Assignment 7 : 
+* Creation of a simple javascript web application for an address book
 
-A demo app for SCSS Webpack configuration.
-
-## Installation
-1. Clone the repository `git clone git@github.com:amuthanarulraj/scss-webpack-template.git`.
-2. Navigate to project directory `cd scss-webpack-template`.
-3. Run `npm install`.
-
-## Build
-Default build mode is `production`. This will build `index.html `, `main.js` and `main.css` files in the `dist` directory.
-1. To build the app, run `npm run build`.
-2. Launh the app by opening the file `./dist/index.html` in a browser.
-
-## Development Server
-Follow below steps to lauch the app using `webpack-dev-server`.
-1. Run `npm start` or `npm run start`.
-2. This will open the app on a browser window.
-
-## License
-[MIT License](https://opensource.org/licenses/MIT)
+## Project Description : 
+* Creating a simple javascript web application for an address book. Rxjs observables are used for adding the event litseners. An Observable is a unique Object similar to a Promise that can help manage async code. Observables are not part of the JavaScript language yet but are being proposed to be added to the language.fetch api/XHR requests are used to make ajax call to the server and handle response using promise.The Promise object represents the eventual completion (or failure) of an asynchronous operation, and its resulting value. 
 
 
+## Requirement :
+* Visual Studio code and a web browser which supports ES6. 
+
+
+## How to run :
+* STEPS :
+1. Open a git bash shell
+2. Execute a command "https://github.com/neu-mis-info6150-spring-2019/assignment-7-navinraman"
+3. Open files with visual studio code in a new workspace. 
+4. Open a new teminal by right clicking on project folder.
+5. Execute a command "npm init" and press enter until it asks you "Is this OK"
+6. Execute a command "npm install webpack --save-dev" and press enter
+7. Execute a command "npm run build" and press enter
+8. When system asks "Do you want to install 'webpack-cli' (yes/no)" then type "YES"
+9. You will see a for successful build
+10.Execute a command "npm install --save express and press enter
+11.Execute a command "npm install --save -dev nodemon and press enter
+12.Execute a command "npm start 
+13. Execute a command " npm install --save morgan " and press enter 
+14.Execute the command " npm start " again and execute " npm install --save mongoose" to install mongoose.
+15. Right click on index.html and click on revel in explore
+16. Double click and execute index.html
+17. Enjoy the buid of an address book Application.
--- src/main.js
+++ src/main.js
@@ -2,42 +2,46 @@ import './../scss/main.scss';
 import { RSA_X931_PADDING } from 'constants';
 let Rx = require('rxjs');
 
-// example {id:1592304983049, title: 'Deadpool', year: 2015}
+// data entry (name n phone details and email)
 const addcontact = (ev)=>{
-    ev.preventDefault();  //to stop the form submitting
-    let contact = { 
-        fname: document.getElementById('fname').value,
-        lname: document.getElementById('lname').value,
-        email: document.getElementById('email').value,
-        number: document.getElementById('number').value
-      }
-    //contacts.push(contact);
-    document.forms[0].reset(); // to clear the form for the next entries
-    //document.querySelector('form').reset();
-    //for display purposes only
-    console.warn('added' , {contact} );
-    // let pre = document.querySelector('#msg pre');
-    // pre.textContent = '\n' + JSON.stringify(contact, '\t', 2);
-    //saving to localStorage
-    localStorage.setItem('MycontactList', JSON.stringify(contact) );
+  ev.preventDefault();  //to stop the form submitting
+  let contact = { 
+    fname: document.getElementById('fname').value,
+    lname: document.getElementById('lname').value,
+    email: document.getElementById('email').value,
+    number: document.getElementById('number').value
+  }
+    
+document.forms[0].reset(); // to clear the form for the next entries
+  
+console.warn('added' , {contact} ); //for display purposes only
+  
+localStorage.setItem('MycontactList', JSON.stringify(contact) ); //saving to localStorage
 
-    fetch("http://localhost:3000/addressBook/",{
-  method: "POST",
-  headers:{
-   'Content-Type':'application/json',
-   mode: 'cors'
-  },
-  body: JSON.stringify(contact)
-})
-.then(function(response){
-  response.json();
-}).catch((err)=>{
-  console.log('Error:',err.message);
-});
+// for posting the data to the database
+fetch("http://localhost:3000/addressBook/", 
+  {
+    method: "POST",
+    headers:
+    {
+    'Content-Type':'application/json',
+    mode: 'cors'
+    },
+    body: JSON.stringify(contact)
+    })
+    .then(function(response){
+    response.json();
+    }).catch((err)=>{
+    console.log('Error:',err.message);
+  });
 }
+
+// adding event listeners for button
 document.addEventListener('DOMContentLoaded', ()=>{
     document.getElementById('submit-btn').addEventListener('click', addcontact);
 });
+
+//funtion for table
 function populateTable(contactlist){
   for( let i=0;i<contactlist.length; i++){
   const contact = (contactlist[i]);
@@ -47,8 +51,9 @@ function populateTable(contactlist){
   }
 }
 
-
 let table = document.querySelector('table');
+
+// function to add row
 function addRow(table, name, id, position) {
   const row = table.insertRow(position);
   const cell1 = row.insertCell(0);
@@ -60,79 +65,40 @@ function addRow(table, name, id, position) {
   cell2.appendChild(button);
   cell2.setAttribute("name", id);
   const view$ = Rx.fromEvent(button,'click');
-let id1 = button.parentElement.getAttribute('name');
+  let id1 = button.parentElement.getAttribute('name');
   view$.subscribe(() =>{fetchcontact(id1)});
 }
 
+// funtion to fetch a specific contact
 function fetchcontact(id){
-  //console.log(id);
-  // contactcard.classList.toggle("hidden");
-  fetch('http://localhost:3000/addressBook/' +`${id}`, {
-    method: 'GET',
-    headers: {
-    "Content-Type": "application/json",
-    mode: 'cors'
-  }, })
+  
+  fetch('http://localhost:3000/addressBook/' +`${id}`, 
+    {
+      method: 'GET',
+      headers: {
+      "Content-Type": "application/json",
+      mode: 'cors'
+    },
+  })
   .then(response => {response.json()
   console.log(JSON.stringify(response))})
   .catch(error => console.error('Error:', error))
-    // console.log(typeof newContact);
-//     console.log(newContact);
-// })
 }
 
-
-
-  let contactlist = [];
-    let req  = new Request(`http://localhost:3000/addressBook`,{
-    method : 'GET',
-    headers: {
-           'Content-Type':'application/json'
-          },
-          mode: 'cors'
+//requesting the data
+let contactlist = [];
+let req  = new Request(`http://localhost:3000/addressBook`,
+{
+  method : 'GET',
+  headers: {'Content-Type':'application/json',mode: 'cors'}
 });
+
+//fetching the data
 fetch(req)
 .then(response => response.json())
-  .catch(error => console.error('Error:', error))
-  .then(response =>   {
-    contactlist=response;
-    populateTable(contactlist);
-    console.log(contactlist);
-  })
-  // .then(function (response) {
-  //   return response.json()
-  // })
-  // .then(function (data) {
-  //   console.log('the data', data);
-  //   contactlist = data;
-  //   populateTable(contactlist);
-  // })
-  // .catch((err)=>{
-  //   console.log('Error:',err.message);
-  // })
-
-  
-
-  
-                
-                  //document.getElementById('result').innerHTML = details;
-
-// fetch('https://jsonplaceholder.typicode.com/users')
-//         .then((res) => { return res.json() })
-//         .then((data) => {
-//             let result = `<h2> Random User Info From Jsonplaceholder API</h2>`;
-//             data.forEach((user) => {
-//                 const { id, name, email, address: { city, street } } = user
-//                 result +=
-//                     `<div>
-//                      <h5> User ID: ${id} </h5>
-//                          <ul class="w3-ul">
-//                              <li> User Full Name : ${name}</li>
-//                              <li> User Email : ${email} </li>
-//                              <li> User Address : ${city}, ${street} </li>
-//                          </ul>
-//                       </div>`;
-//                         document.getElementById('result').innerHTML = result;
-//                     });
-//                 })
-// }
+.catch(error => console.error('Error:', error))
+.then(response =>   {
+  contactlist=response;
+  populateTable(contactlist);
+  console.log(contactlist);
+})
